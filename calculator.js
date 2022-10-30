14e5382d4ef03120c8da7fd954067741b15c89b0
Josh Hall <67877014+joshuarhall@users.noreply.github.com>
2022-10-29 20:29:03 -0700

    Update calculator.js
    adding the first bits of js code to the calculator...
    It's garbage right now b/c I don't know how to do inputs, but I can fix that later.

--- calculator.js
+++ calculator.js
@@ -1,1 +1,136 @@
-//the brain behind the calculator I'm making
+/* 
+Create a function for each of the buttons on the calculator. 
+This is probably the wrong way to do it, as I should be saving inputs into memory and then doing math on those.
+*/
+
+//addition buttons
+function add0(input) {
+    console.log(input * 0);
+}
+function add1(input) {
+    console.log(input * 1);
+}
+function add2(input) {
+    console.log(input * 2);
+}
+function add3(input) {
+    console.log(input * 3);
+}
+function add4(input) {
+    console.log(input * 4);
+}
+function add5(input) {
+    console.log(input * 5);
+}
+function add6(input) {
+    console.log(input * 6);
+}
+function add7(input) {
+    console.log(input * 7);
+}
+function add8(input) {
+    console.log(input * 8);
+}
+function add9(input) {
+    console.log(input * 9);
+}
+
+//subtraction buttons
+function minus0(input) {
+    console.log(input * 0);
+}
+function minus1(input) {
+    console.log(input * 1);
+}
+function minus2(input) {
+    console.log(input * 2);
+}
+function minus3(input) {
+    console.log(input * 3);
+}
+function minus4(input) {
+    console.log(input * 4);
+}
+function minus5(input) {
+    console.log(input * 5);
+}
+function minus6(input) {
+    console.log(input * 6);
+}
+function minus7(input) {
+    console.log(input * 7);
+}
+function minus8(input) {
+    console.log(input * 8);
+}
+function minus9(input) {
+    console.log(input * 9);
+}
+
+
+//multiplication buttons
+function mult0(input) {
+    console.log(input * 0);
+}
+function mult1(input) {
+    console.log(input * 1);
+}
+function mult2(input) {
+    console.log(input * 2);
+}
+function mult3(input) {
+    console.log(input * 3);
+}
+function mult4(input) {
+    console.log(input * 4);
+}
+function mult5(input) {
+    console.log(input * 5);
+}
+function mult6(input) {
+    console.log(input * 6);
+}
+function mult7(input) {
+    console.log(input * 7);
+}
+function mult8(input) {
+    console.log(input * 8);
+}
+function mult9(input) {
+    console.log(input * 9);
+}
+
+//division buttons
+function div0(input) {
+    console.log(input * 0);
+}
+function div1(input) {
+    console.log(input * 1);
+}
+function div2(input) {
+    console.log(input * 2);
+}
+function div3(input) {
+    console.log(input * 3);
+}
+function div4(input) {
+    console.log(input * 4);
+}
+function div5(input) {
+    console.log(input * 5);
+}
+function div6(input) {
+    console.log(input * 6);
+}
+function div7(input) {
+    console.log(input * 7);
+}
+function div8(input) {
+    console.log(input * 8);
+}
+function div9(input) {
+    console.log(input * 9);
+}
+
+
+
